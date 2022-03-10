f8a3d203d7e30184eed48cf8f2623e20aeb3534d
Thomas <tdelorme@gaming.tech>
2022-03-10 10:11:47 +0100

    pull thib 10/03

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -20,7 +20,7 @@ function Menu(props) {
         </Nav.Item>
         <Nav.Item className="justify-content-end">
           <Dropdown>
-            <Dropdown.Toggle className="dropdown-toggle">
+            <Dropdown.Toggle className="dropdown-toggle" id={'dropdown-button-drop-sart'}>
               <Link className="navbar-icon" to='/Store'><FontAwesomeIcon icon={faCartShopping} size='2x' /></Link>
             </Dropdown.Toggle>
 
